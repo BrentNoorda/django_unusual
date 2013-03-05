b28b1ff2733e1e87dfc6e549680746436c8022c4
Brent Noorda <brent.noorda@gmail.com>
2013-03-04 16:23:39 -0800

    get admin woking in gunicorn. what a PITA!

--- django_unusual/urls.py
+++ django_unusual/urls.py
@@ -5,6 +5,11 @@ import django_unusual.views
 from django.contrib import admin
 admin.autodiscover()
 
+# used for delivering media files from gunicorn - see gunicorn note below
+import os
+import django
+admin_media_path = os.path.join(django.__path__[0], 'contrib', 'admin', 'static', 'admin')
+
 urlpatterns = patterns('',
 
     url(r'^(.*\.mako)$', django_unusual.views.show_mako_page),
@@ -21,4 +26,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # when running under gunicorn, the above stops working, but still want to deliver media files, so
+    # borrow a little from http://stackoverflow.com/questions/6984672/django-admin-site-not-formatted
+    # and a little from https://docs.djangoproject.com/en/dev/howto/static-files/ to get this
+    url(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': admin_media_path,
+    }),
 )
